4573e65a6c1cb6a29abaadf875e998f08c3fc52c
xbedoya <ximena.bedoya@utp.edu.co>
2026-05-06 17:31:23 -0500

    feat(back-wallet): add endpoint for retrieving receipts list

--- ProjectPanamaBackend/routes/wallet.py
+++ ProjectPanamaBackend/routes/wallet.py
@@ -10,3 +10,7 @@ async def get_vehicle_wallet_info(company_code: str, vehicle_number: str, driver
 @wallet_router.get("/wallet/vehicle-driver-info/{company_code}/{vehicle_number}/", tags=["Wallet"])
 async def get_vehicle_and_driver_info(company_code: str, vehicle_number: str):
   return await vehicle_and_driver_info(company_code, vehicle_number)
+
+@wallet_router.get("/wallet/receipts/{company_code}/{vehicle_number}/{driver_number}/", tags=["Wallet"])
+async def get_receipts_list(company_code: str, vehicle_number: str, driver_number: str):
+  return await receipts_list(company_code, vehicle_number, driver_number)
